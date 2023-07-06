2c78e3520950fd14e88610b5458b8df39fefbfa5
alexb454 <71828880+alexb454@users.noreply.github.com>
2023-07-06 15:15:17 -0230

    seed is working, json file is on postgres

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//import express from 'express';
-//import argon2 from 'argon2';
-//import dotenv from 'dotenv';
-
 const express = require('express');
 
 const app = express();
--- seed/seed.js
+++ seed/seed.js
@@ -4,6 +4,10 @@
 const pool = require('pg').Pool;
 const data = require('../data/recipes.json');
 
+const recipes = data;
+recipes.forEach(recipe => {
+    const {id, title, ingrediants, instructions, times, image} = recipe
+})
 
 const db = new pool({
     user: 'postgres',
@@ -17,9 +21,9 @@ async function Seed() {
     try {
         await db.connect();
 
-        for (const recipedb of data) {
-            await db.query('INSERT INTO recipe (id, title, ingredients, instructions, times, image) VALUES ($1, $2, $3, $4, $5, $6)', [
-                recipedb.body.id, recipedb.body.title, recipedb.body.ingredients, recipedb.body.instructions, recipedb.body.times, recipedb.body.image
+        for (const recipedb of recipes) {
+            await db.query('INSERT INTO recipe (id, title, ingredients, instructions, times, image) VALUES ($1, ARRAY[$2], $3, $4, $5, $6)', [
+                recipedb.id, recipedb.title, recipedb.ingredients, recipedb.instructions, recipedb.times, recipedb.image
             ]);
         }
         console.log('Recipes are in the database!');
