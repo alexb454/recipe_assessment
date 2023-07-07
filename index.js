3d8996ba5a6e71171fddbb7f9499709fa1fe485d
alexb454 <71828880+alexb454@users.noreply.github.com>
2023-07-07 14:19:33 -0230

    fixed the app.post

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const data = require('./database/db.js')
 const app = express();
 const PORT = 3000
 
-app.post("/allrecipe", data.all_recipes)
+app.post("/allrecipe", (req, res) => {
+    data.find()
+    .then(recipe => res.json(recipe))
+    .catch(error => res.status(500).json({ error: 'Error getting the info'}));
+})
 
 app.listen(PORT, () => {
     console.log(`Server is started on ${PORT}`)
