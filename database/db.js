06423cc571b16241f7053536610457e9fba8cdb5
alexb454 <71828880+alexb454@users.noreply.github.com>
2023-07-07 10:40:17 -0230

    not using these files and fixed up db file

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
-import pool from 'pg';
+const pool = require('pg').Pool;
 
 const db = new pool({
     user: 'postgres',
@@ -9,11 +9,28 @@ const db = new pool({
 })
 
 //post
-const create_recipes = await db.query(
-    'INSERT INTO recipe (title, ingredients, instructions, times, image) VALUES($1, $2, $3, $4, $5) RETURNING *'
-    [req.body.title, req.body.ingredients, req.body.instructions, req.body.times, req.body.image]
-)
-console.log('created recipe successfully')
+// const create_recipes = await db.query(
+//     'INSERT INTO recipe (title, ingredients, instructions, times, image) VALUES($1, $2, $3, $4, $5) RETURNING *'
+//     [req.body.title, req.body.ingredients, req.body.instructions, req.body.times, req.body.image]
+// )
+// console.log('created recipe successfully')
+
+async function Create() {
+    try {
+        await db.connect();
+
+        for (const createdb of create_info) {
+            await db.query('INSERT INTO recipe (id, title, ingredients, instructions, times, image) VALUES ($1, $2, $3, $4, $5, $6)', [
+                createdb.id, createdb.title, createdb.ingredients, createdb.instructions, createdb.times, createdb.image
+            ]);
+        }
+        console.log('New recipes are in the database!');
+    } catch (error) {
+        console.error('sadness happend, issue with inserting data:', error);
+    } finally {
+        await db.end();
+    }
+}
 
 //get
 // const all_recipes = await db()
@@ -26,3 +43,5 @@ console.log('created recipe successfully')
 // const update_recipe = await db()
 //delete
 // const delete_recipe = await db()
+
+Create();
